6a9160e6579b0a6f5ab25b508328669c84ba271e
Dawit <teklebrhandawit309@gmail.com>
2026-05-25 17:47:21 +0300

    feat: add SkillSwap theme tokens

---
+++ lib/src/core/theme/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const primaryGreen = Color(0xFF0F766E);
+  static const primaryDark = Color(0xFF115E59);
+  static const accentGold = Color(0xFFF4B740);
+  static const softGold = Color(0xFFFEF3C7);
+  static const backgroundWarm = Color(0xFFFAF7F0);
+  static const cardWhite = Color(0xFFFFFFFF);
+  static const textDark = Color(0xFF1F2933);
+  static const textGray = Color(0xFF6B7280);
+  static const border = Color(0xFFE5E7EB);
+  static const success = Color(0xFF16A34A);
+  static const warning = Color(0xFFF59E0B);
+  static const danger = Color(0xFFDC2626);
+
+  static const tealTint = Color(0xFFE6F5F3);
+  static const softBlue = Color(0xFFEAF2FF);
+  static const mutedSurface = Color(0xFFF3F4F6);
+}
